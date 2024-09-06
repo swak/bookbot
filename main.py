b202e342cca4e220a9dffdc488022698087931ad
swak <swak@theswak.com>
2024-09-06 11:56:03 -0700

    added features to read text files and analyze word count and characters

---
+++ main.py
@@ -0,0 +1,55 @@
+# main.py
+
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+
+    print("--- End report ---")
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char":  ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def get_chars_dict(text):
+    char_freq = {}
+    filtered_text = text.lower()
+    for i in filtered_text:
+        if i in char_freq:
+            char_freq[i] += 1
+        else:
+            char_freq[i] = 1
+    return char_freq
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+if __name__ == "__main__":
+    main()
